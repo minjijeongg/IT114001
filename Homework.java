f937fd0e66c125a5708fb66e4d6450b131dfac1a
Minji Jeong <mj323@njit.edu>
2020-09-17 15:03:40 -0400

    Java Data hw

---
+++ Homework.java
@@ -0,0 +1,11 @@
+public class Homework {
+	public static void main(String[] args) {
+		String name = "Minji";
+		System.out.println(name);
+		int age = 23;
+		System.out.println(age);
+		float number = 23.4f;
+		System.out.println(number);
+		
+	}
+}
